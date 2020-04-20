5221e5d3d779dc33f9620278f17e1af5ade9dcb5
radhe-shyam <jangir.radheyshyam@gmail.com>
2020-04-20 20:00:30 +0530

    Updates

---
+++ precache-manifest.16b97a0185d8b6ca006351ed3599714d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e9e11f7f826bd9231d9400ee6f0b059",
+    "url": "/covid/index.html"
+  },
+  {
+    "revision": "851b9f05fec050beaeb8",
+    "url": "/covid/static/css/main.a5334232.chunk.css"
+  },
+  {
+    "revision": "b0bc324f28765865d161",
+    "url": "/covid/static/js/2.616236ba.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid/static/js/2.616236ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "851b9f05fec050beaeb8",
+    "url": "/covid/static/js/main.9ff17403.chunk.js"
+  },
+  {
+    "revision": "29d887c2aea3fb65dc6d",
+    "url": "/covid/static/js/runtime-main.b3dccd4d.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/covid/static/media/covid.d7265326.png"
+  }
+]);
